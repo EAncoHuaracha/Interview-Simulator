3de59c7195d5512b16cefa4aa45ba6964880e2d9
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2024-07-28 14:24:26 -0500

    Changes in the menu

---
+++ Assets/Scripts/InterviewOption.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Cargar escenas.
+
+public class InterviewOption : MonoBehaviour
+{
+    // Estas funciones se llamarán cuando se haga clic en el respectivo botón
+
+    private void entrevistaDesarrollador()
+    {
+        // Cambia a la escena especificada cuando el objeto es clicado
+         SceneManager.LoadScene("InterviewAccount"); //Cambio de escena apra desarrolladores(Programadores)
+    }
+    public void entrevistaContador()
+    {
+        SceneManager.LoadScene("InterviewAccount"); // Cambio de escena para contadores
+    }
+    public void entrevistaPsicologo()
+    {
+        SceneManager.LoadScene("InterviewAccount"); // Cambio de escena para psicologos
+    }
+}
