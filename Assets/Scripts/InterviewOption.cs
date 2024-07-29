589bd0c452576cd65b36401d13a0219495d5b0c6
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2024-07-29 13:16:41 -0500

    Canva update

--- Assets/Scripts/InterviewOption.cs
+++ Assets/Scripts/InterviewOption.cs
@@ -19,4 +19,12 @@ public class InterviewOption : MonoBehaviour
     {
         SceneManager.LoadScene("InterviewAccount"); // Cambio de escena para psicologos
     }
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 }
